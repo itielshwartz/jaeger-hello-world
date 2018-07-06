c76825b906ea50b8ca3742550169b1a46512f5ad
Itiel <itielshw@gmail.com>
2018-07-06 23:26:11 +0300

    "fix readme and main"

--- README.md
+++ README.md
@@ -1,26 +1,23 @@
-Step 3 Adding jaeger
-====================
+Step 4 Multiple spans
+=====================
 
-In this part we are going to add jaeger to our web-app.
+In this part we are going to add multiple spans to our web-app.
 
 ---
 
-1. We added `tracer.py` -> a jaeger client basic config.
-
-2. Then we added it to the start of our web-server :
+1. We added the span to our app context (so we can refrence it in other functions)
     ```python
-        with tracer.start_span('main-context') as span:
+    with span_in_context(span):
+    ```
+
+2. Then we added new span for both functions (while refrence thd main span):
+    ```python
+    with tracer.start_span("clean_github_data", child_of=get_current_span()) as span:
     ```
 
 To run
 ------
 
-* Run the jaeger all-in-one docker:
-    ```bash
-    docker run -d -p5775:5775/udp -p6831:6831/udp -p6832:6832/udp \
-      -p5778:5778 -p16686:16686 -p14268:14268 -p9411:9411 \
-      jaegertracing/all-in-one:1.5.02
-     ```
 * Run the server
     ```bash
     python main.py
@@ -32,5 +29,6 @@ To run
 Ok now we can check the jaeger UI!
 * Check the Jaeger UI at http://localhost:16686
 
-We can see one request took much more time then the other -
-But which one?
+Great not we can see what function took more time `get_repo_contributors`!
+
+But which request caused it?
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
 import requests
 from flask import Flask
+from opentracing_instrumentation.request_context import get_current_span, span_in_context
 
 from bad_things import bad_thing_0, bad_thing_1
 from tracer import init_tracer
@@ -12,27 +13,30 @@ tracer = init_tracer("gateway")
 
 
 def get_repo_contributors(repo_and_owner):
-    url = get_repo.format(repo_and_owner)
-    bad_thing_0(url)
-    return requests.get(url)
+    with tracer.start_span("get_repo_contributors", child_of=get_current_span()) as span:
+        url = get_repo.format(repo_and_owner)
+        bad_thing_0(url)
+        return requests.get(url)
 
 
 def clean_github_data(repo_contributors):
-    contributors_to_commit = {}
-    bad_thing_1()
-    for contribute in repo_contributors:
-        username = contribute["author"]["login"]
-        contributors_to_commit[username] = sum((week["c"] for week in contribute["weeks"]))
-    return contributors_to_commit
+    with tracer.start_span("clean_github_data", child_of=get_current_span()) as span:
+        contributors_to_commit = {}
+        bad_thing_1()
+        for contribute in repo_contributors:
+            username = contribute["author"]["login"]
+            contributors_to_commit[username] = sum((week["c"] for week in contribute["weeks"]))
+        return contributors_to_commit
 
 
 @app.route("/git/<owner>/<repo>")
 def main(owner=None, repo=None):
     with tracer.start_span('main-context') as span:
-        repo_name = "{}/{}".format(owner, repo)
-        repo_contributors = get_repo_contributors(repo_name).json()
-        contributors_commits = clean_github_data(repo_contributors)
-        return flask.jsonify(**contributors_commits, indent=2)
+        with span_in_context(span):
+            repo_name = "{}/{}".format(owner, repo)
+            repo_contributors = get_repo_contributors(repo_name).json()
+            contributors_commits = clean_github_data(repo_contributors)
+            return flask.jsonify(**contributors_commits, indent=2)
 
 
 if __name__ == "__main__":
